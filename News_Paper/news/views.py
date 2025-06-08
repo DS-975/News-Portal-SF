0000c70b5ad0426f1ec81f5b677c639791ab8565
DS-975 <975ds579@gmail.com>
2025-06-08 20:24:12 +0300

    Добавил таблицу Заголовок, Дата публикации, Техт статьи на страницу /news

--- News_Paper/news/views.py
+++ News_Paper/news/views.py
@@ -3,6 +3,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 
+from datetime import datetime
 
 class PostList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -14,7 +15,23 @@ class PostList(ListView):
     template_name = 'news.html'
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
-    context_object_name = 'text'
+    context_object_name = 'Post'
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_sale'] = "Распродажа в среду!"
+        context['news'] = 'Нет новостей'
+        return context
 
 
 # Вот так мы можем использовать дженерик ListView для вывода списка товаров:
